6bc78c588ec8191c52ca87a3098495005ae0ee40
xtophe xanon <xocon10@gmail.com>
2020-12-18 08:34:04 -0600

    create express server and react client

---
+++ server/server.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+// const mailerRoute = require('./routes/mailerRoute')
+
+const PORT = process.env.PORT || 3001;
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+// app.use('/', mailerRoute)
+
+// serve up static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')))
+}
+
+// serve index.html for any unknown paths:
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'))
+})
+
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
