ef627ad6e687563995e047572f0dcbd49a628209
xtophe xanon <xocon10@gmail.com>
2020-12-27 11:57:17 -0600

    refactor mobile menu and styles

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,41 +1,87 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import { Link, NavLink } from 'react-router-dom'
 
-
 const Navbar = () => {
-  const activeStyle = {
-    backgroundRepeat: 'repeat-x',
-    backgroundImage: 'linear-gradient(to right, currentColor 100%, currentColor 0)',
-    backgroundSize: '1px 1px',
-    backgroundPosition: '0 calc(100% - 0.8em)' 
-	};
-  return (
-    <nav id="navbar" className="container">
-      <div className="logo">
-        <Link to="/">Xtophe Xanon</Link>
-      </div>
-      <ul className="nav-items">
-          <li><NavLink exact to="/" activeStyle={activeStyle}>Imagen</NavLink></li>
-          <li><NavLink to="/contact" activeStyle={activeStyle}>Contact</NavLink></li>
-      </ul>
-    
-      <div className="overlay-wrap">
-        <input type="checkbox" className="toggler" />
-        {/*inner empty div will become hamburger line */}
-        <div className="hamburger"><div></div></div>
-        <div className="menu">
-          <div>
-            <div>
-              <ul>
-                <li><a href="index.html">Work</a></li>
-                <li><a href="contact.html">Contact</a></li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </div>
-    </nav>
-  )
+	const activeStyle = {
+		backgroundRepeat   : 'repeat-x',
+		backgroundImage    : 'linear-gradient(to right, currentColor 100%, currentColor 0)',
+		backgroundSize     : '1px 1px',
+		backgroundPosition : '0 calc(100% - 0.8em)'
+	}
+
+	const [ checked, setChecked ] = useState(false)
+	const checkboxRef = useRef()
+
+	useEffect(() => {
+		if (checkboxRef) {
+			checkboxRef.current.checked = checked
+    }
+  }, [checked])
+  
+  useEffect(() => {  
+    if (checkboxRef.current.checked) {
+      document.body.style.overflow = 'hidden'
+    } else {
+      document.body.style.overflow = 'scroll'
+    }
+  }, [checked])
+
+	function handleMobileClick() {
+		setChecked(false)
+	}
+
+	return (
+		<nav id='navbar' className='container'>
+			<div className='logo'>
+				<Link to='/'>Xtophe Xanon</Link>
+			</div>
+			<ul className='nav-items'>
+				<li>
+					<NavLink exact to='/' activeStyle={activeStyle}>
+						Imagen
+					</NavLink>
+				</li>
+				<li>
+					<NavLink to='/contact' activeStyle={activeStyle}>
+						Contact
+					</NavLink>
+				</li>
+			</ul>
+
+			<div className='overlay-wrap'>
+				<input
+					type='checkbox'
+					className='toggler'
+					onClick={() => setChecked(!checked)}
+					ref={checkboxRef}
+				/>
+				{/*inner empty div will become hamburger line */}
+				<div className='hamburger'>
+					<div />
+				</div>
+				<div className='menu'>
+					<div>
+						<div>
+							<ul>
+								<li>
+									{/* <NavLink exact to='/' onClick={() => setChecked(false)}> */}
+									<NavLink exact to='/' onClick={() => handleMobileClick()}>
+										Imagen
+									</NavLink>
+								</li>
+								<li>
+									{/* <NavLink to='/contact' onClick={() => setChecked(false)}> */}
+									<NavLink to='/contact' onClick={() => handleMobileClick()}>
+										Contact
+									</NavLink>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</nav>
+	)
 }
 
 export default Navbar
