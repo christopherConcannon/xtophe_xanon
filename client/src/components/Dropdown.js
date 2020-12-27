80a7a727cb56794f5a5ea4283a2a7c55510deffc
xtophe xanon <xocon10@gmail.com>
2020-12-27 07:00:04 -0600

    stash dropdown

--- client/src/components/Dropdown.js
+++ client/src/components/Dropdown.js
@@ -4,35 +4,21 @@ import { Link } from 'react-router-dom'
 const Dropdown = () => {
 	const [ showDropdown, setShowDropdown ] = useState(false)
 
-	const renderLinks = () => {
-		return (
-			<ul>
-				<li>
-					<Link to='/bistrot'>bistrot</Link>
-				</li>
-				<li>
-					<Link to='/quixote'>quixote</Link>
-				</li>
-				<li>
-					<Link to='/myco'>myco</Link>
-				</li>
-				<li>
-					<Link to='/eclectica'>eclectica</Link>
-				</li>
-			</ul>
-		)
-	}
-
 	return (
-		<li className='nav-dropdown'>
-			<button
-				onMouseEnter={() => setShowDropdown(true)}
-				onMouseLeave={() => setShowDropdown(false)}
-			>
-				Imagen
-			</button>
-      { showDropdown && renderLinks()}
-		</li>
+		<ul className='dropdown-menu'>
+			<li>
+				<Link to='/bistrot'>bistrot</Link>
+			</li>
+			<li>
+				<Link to='/quixote'>quixote</Link>
+			</li>
+			<li>
+				<Link to='/myco'>myco</Link>
+			</li>
+			<li>
+				<Link to='/eclectica'>eclectica</Link>
+			</li>
+		</ul>
 	)
 }
 
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,43 +1,74 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link, NavLink } from 'react-router-dom'
 
 import Dropdown from './Dropdown'
 
 const Navbar = () => {
-  const activeStyle = {
-    backgroundRepeat: 'repeat-x',
-    backgroundImage: 'linear-gradient(to right, currentColor 100%, currentColor 0)',
-    backgroundSize: '1px 1px',
-    backgroundPosition: '0 calc(100% - 0.8em)' 
-	};
-  return (
-    <nav id="navbar" className="container">
-      <div className="logo">
-        <Link to="/">Xtophe Xanon</Link>
-      </div>
-      <ul className="nav-items">
-          {/* <li><NavLink exact to="/" activeStyle={activeStyle}>Imagen</NavLink></li> */}
-          <Dropdown />
-          <li><NavLink to="/contact" activeStyle={activeStyle}>Contact</NavLink></li>
-      </ul>
-    
-      <div className="overlay-wrap">
-        <input type="checkbox" className="toggler" />
-        {/*inner empty div will become hamburger line */}
-        <div className="hamburger"><div></div></div>
-        <div className="menu">
-          <div>
-            <div>
-              <ul>
-                <li><a href="index.html">Work</a></li>
-                <li><a href="contact.html">Contact</a></li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </div>
-    </nav>
-  )
+	const activeStyle = {
+		backgroundRepeat   : 'repeat-x',
+		backgroundImage    : 'linear-gradient(to right, currentColor 100%, currentColor 0)',
+		backgroundSize     : '1px 1px',
+		backgroundPosition : '0 calc(100% - 0.8em)'
+	}
+
+	const [ showDropdown, setShowDropdown ] = useState(false)
+
+	return (
+		<nav id='navbar' className='container'>
+			<div className='logo'>
+				<Link to='/'>Xtophe Xanon</Link>
+			</div>
+			<ul className='nav-items'>
+				{/* <li><NavLink exact to="/" activeStyle={activeStyle}>Imagen</NavLink></li> */}
+				<li
+					className='nav-dropdown-container'
+					onMouseEnter={() => setShowDropdown(true)}
+					onMouseLeave={() => setShowDropdown(false)}
+				>
+					<button>Imagen</button>
+					{showDropdown && <Dropdown />}
+				</li>
+
+				<li>
+					<NavLink to='/contact' activeStyle={activeStyle}>
+						Contact
+					</NavLink>
+				</li>
+			</ul>
+
+			<div className='overlay-wrap'>
+				<input type='checkbox' className='toggler' />
+				{/*inner empty div will become hamburger line */}
+				<div className='hamburger'>
+					<div />
+				</div>
+				<div className='menu'>
+					<div>
+						<div>
+							<ul>
+								{/* <li>
+									<a href='index.html'>Work</a>
+								</li> */}
+								<li
+									className='nav-dropdown-container'
+									onMouseEnter={() => setShowDropdown(true)}
+									onMouseLeave={() => setShowDropdown(false)}
+								>
+									<button>Imagen</button>
+									{showDropdown && <Dropdown />}
+								</li>
+								<li>
+									<NavLink to='/contact' activeStyle={activeStyle}>
+										Contact
+									</NavLink>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</nav>
+	)
 }
 
 export default Navbar
